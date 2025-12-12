4986567287482dcc4d9e48424bf35dd576db1bb8
Zach Bugay <zacharybugay@microsoft.com>
2025-12-12 07:19:03 -0500

    Update AppData/Local/nvim/init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,3 +1,13 @@
 require("myluamodule")
 
-vim.wo.number 	= true
+vim.opt.number 		= true
+vim.opt.showmatch 	= true
+vim.opt.hlsearch 	= true
+vim.opt.tabstop 	= 4
+vim.opt.softtabstop 	= 4
+vim.opt.autoindent 	= true
+vim.opt.cc 		= 100  -- Show a vertical bar
+vim.opt.ttyfast 	= true -- Speed up scrolling
+vim.opt.list 		= true -- Show White Space
+vim.opt.listchars 	= 'tab:» ,lead:•,trail:•'
+vim.opt.syntax 		= "on"
