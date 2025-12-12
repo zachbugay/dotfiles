587fb7622384dbc3d50c29cc231e14710186d7ae
Zach Bugay <zacharybugay@microsoft.com>
2025-12-12 13:08:48 -0500

    Update AppData/Local/nvim/init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,17 +1,17 @@
 -- require("myluamodule")
 
-vim.opt.number 		= true
-vim.opt.showmatch 	= true
-vim.opt.hlsearch 	= true
-vim.opt.tabstop 	= 4
-vim.opt.softtabstop 	= 4
-vim.opt.expandtab 	= true -- Convert tabs to spaces
-vim.opt.smartindent 	= true -- Automatically indent new lines
-vim.opt.ttyfast 	= true -- Speed up scrolling
-vim.opt.wrap 		= false -- Disable linewrap
-vim.opt.cursorline 	= true -- Highlight the current line
-vim.opt.list 		= true -- Show White Space
-vim.opt.listchars 	= {
+vim.opt.number          = true
+vim.opt.showmatch       = true
+vim.opt.hlsearch        = true
+vim.opt.tabstop         = 4
+vim.opt.softtabstop     = 4
+vim.opt.expandtab       = true -- Convert tabs to spaces
+vim.opt.smartindent     = true -- Automatically indent new lines
+vim.opt.ttyfast         = true -- Speed up scrolling
+vim.opt.wrap            = false -- Disable linewrap
+vim.opt.cursorline      = true -- Highlight the current line
+vim.opt.list            = true -- Show White Space
+vim.opt.listchars       = {
   space = "·",     -- Middle dot for spaces
   tab = "→ ",      -- Arrow for tabs (must be at least 2 chars)
   trail = "•",     -- Bullet for trailing spaces
@@ -19,7 +19,7 @@ vim.opt.listchars 	= {
   precedes = "⟨",  -- Character for text that precedes the screen
   nbsp = "␣"       -- Non-breaking space
 }
-vim.opt.syntax 		= "on"
+vim.opt.syntax          = "on"
 
 vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
