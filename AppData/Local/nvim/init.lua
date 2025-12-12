8101e5b08609551c4a7c581cda5db0e9a2c83626
Zach Bugay <zacharybugay@microsoft.com>
2025-12-12 07:34:58 -0500

    update init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,13 +1,27 @@
-require("myluamodule")
+-- require("myluamodule")
 
 vim.opt.number 		= true
 vim.opt.showmatch 	= true
 vim.opt.hlsearch 	= true
 vim.opt.tabstop 	= 4
 vim.opt.softtabstop 	= 4
-vim.opt.autoindent 	= true
+vim.opt.expandtab 	= true -- Convert tabs to spaces
+vim.opt.smartindent 	= true -- Automatically indent new lines
 vim.opt.cc 		= 100  -- Show a vertical bar
 vim.opt.ttyfast 	= true -- Speed up scrolling
+vim.opt.wrap 		= false -- Disable linewrap
+vim.opt.cursorline 	= true -- Highlight the current line
 vim.opt.list 		= true -- Show White Space
-vim.opt.listchars 	= 'tab:» ,lead:•,trail:•'
+vim.opt.listchars 	= {
+  space = "·",     -- Middle dot for spaces
+  tab = "→ ",      -- Arrow for tabs (must be at least 2 chars)
+  trail = "•",     -- Bullet for trailing spaces
+  extends = "⟩",   -- Character for text that extends beyond the screen
+  precedes = "⟨",  -- Character for text that precedes the screen
+  nbsp = "␣"       -- Non-breaking space
+}
 vim.opt.syntax 		= "on"
+
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
