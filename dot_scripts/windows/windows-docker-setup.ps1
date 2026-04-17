4110e1dc6329b24a3c96a2937d38b0135da6a2a3
Zach Bugay <zacharybugay@microsoft.com>
2026-04-17 09:52:00 -0400

    fix: refactor Docker installation script for improved readability and maintainability

--- dot_scripts/windows/windows-docker-setup.ps1
+++ dot_scripts/windows/windows-docker-setup.ps1
@@ -7,9 +7,7 @@ param (
 )
 
 function Test-IsElevated {
-  return (New-Object Security.Principal.WindowsPrincipal(
-      [Security.Principal.WindowsIdentity]::GetCurrent()))
-  .IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+  return (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 function Install-Docker {
@@ -24,11 +22,11 @@ function Install-Docker {
   Write-Host 'Installing Docker...';
   $installDirectory = [System.IO.Path]::Combine($Env:LOCALAPPDATA, 'bugay-docker-installer')
   $destinationDirectory = [System.IO.Path]::Combine($Env:USERPROFILE, 'scoop', 'shims')
-  
+
   if (!(Test-Path -Path $destinationDirectory)) {
     New-Item -ItemType Directory -Path $installDirectory
   }
-  
+
   $git = Get-Command git -ErrorAction SilentlyContinue
   $go = Get-Command go -ErrorAction SilentlyContinue
 
@@ -40,14 +38,14 @@ function Install-Docker {
   }
 
   $dockerRepoPath = [System.IO.Path]::Combine($installDirectory, 'cli');
-  
+
   if (!(Test-Path -Path $dockerRepoPath)) {
     git -C $dockerRepoPath clone git@github.com:docker/cli.git;
   }
 
   # Ensure we've got the latest updates
   git -C $dockerRepoPath fetch origin;
-  
+
   # Pin to the latest commit SHA on the default branch
   $commit = (git -C $dockerRepoPath rev-parse origin/master).Trim()
   $shortSha = $commit.Substring(0, 12)
@@ -65,6 +63,7 @@ function Install-Docker {
   # Build with modules on, using the vendor directory
   $env:GO111MODULE = 'on'
   $env:GOTOOLCHAIN = 'local'
+  
   # https://github.com/docker/cli
   $dockerExe=[System.IO.Path]::Combine($dockerRepoPath, 'build', 'docker.exe')
   go build -C $dockerRepoPath -mod=vendor -o $dockerExe -ldflags "-X github.com/docker/cli/cli/version.Version=$version -X github.com/docker/cli/cli/version.GitCommit=$commit" github.com/docker/cli/cmd/docker
@@ -88,10 +87,10 @@ function Install-BugayInitializeDocker {
     param (
       [string]$Description,
       [string]$TaskName,
-      [string]$BinPath
+      [string]$BinaryPath
     )
 
-    $action = New-ScheduledTaskAction -Execute $BinPath
+    $action = New-ScheduledTaskAction -Execute $BinaryPath
     $trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
     $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Limited
     $settings = New-ScheduledTaskSettingsSet `
@@ -108,23 +107,24 @@ function Install-BugayInitializeDocker {
 
     Start-ScheduledTask -TaskName $TaskName -TaskPath '\Bugay\'
   }
+
   # TODO: Dynamic, based on if I have a dev drive, or if in ~\source\repos directory
   $bugayInitializeDockerLocation = 'D:\zacharybugay\source\repos\developer-environment\Bugay.Initialize.Docker'
-
+  Write-Host "Build value: $Build"
   if ($Build) {
     Write-Host 'Building the binary...'
     dotnet clean
     dotnet publish -c release -r win-arm64
   }
-  
+
   $installDirectory = [System.IO.Path]::Combine($Env:LOCALAPPDATA, 'bugay-docker-installer', 'bin')
-  if ($false -eq (Test-Path $installDirectory -ErrorAction SilentlyContinue)) {
+  if ($False -eq (Test-Path $installDirectory -ErrorAction SilentlyContinue)) {
     New-Item -Type Directory -Path $installDirectory
   }
-  
+
   Write-Host 'Installing the binary...'
   $installPath = [System.IO.Path]::Combine($installDirectory, 'Bugay.Initialize.Docker.exe')
-  
+
   $exePath = [System.IO.Path]::Combine($bugayInitializeDockerLocation, 'src', 'bin', 'Release', 'net10.0', 'win-arm64', 'publish', 'Bugay.Initialize.Docker.exe')
   Write-Host "exePath is: $exePath"
   $desc = 'Starts WSL in the background so the Host can communicate with the Docker daemon.'
@@ -134,17 +134,14 @@ function Install-BugayInitializeDocker {
   if ($null -ne $existingTask) {
     Write-Host 'Removing existing scheduled task...'
     Get-ScheduledTask -TaskPath '\Bugay\' | Stop-ScheduledTask
-    Unregister-ScheduledTask -TaskName $taskName -TaskPath '\Bugay\' -Confirm:$false
+    Unregister-ScheduledTask -TaskName $taskName -TaskPath '\Bugay\' -Confirm:$False
+      if ([System.IO.File]::Exists($installPath)) {
+        Write-Host 'Removing old binary...'
+        Remove-Item -Path $installPath
+      }
+    Copy-Item -Path $exePath -Destination $installPath
+    Register-BugayInitializeDockerTask -Description $desc -TaskName $taskName -BinaryPath $installPath
   }
-
-  if ([System.IO.File]::Exists($installPath)) {
-    Write-Host 'Removing old binary...'
-    Remove-Item -Path $installPath
-  }
-
-  Copy-Item -Path $exePath -Destination $installPath
-  Install-BugayInitializeDocker -Build
-  Register-BugayInitializeDockerTask -Description $desc -TaskName $taskName -BinPath $installPath
 }
 
 function Initialize-LocalDockerContext {
@@ -168,19 +165,30 @@ function Initialize-LocalDockerContext {
   $sshConfigPath = [System.IO.Path]::Combine($Env:USERPROFILE, '.ssh', 'config')
   # TODO: This is different now.
   if (![System.IO.File]::Exists($sshConfigPath)) {
-@'
+    $content = @"
 Host wsl
     HostName $wslIp
     User zacharybugay
     IdentityFile ~/.ssh/local_wsl
     Port 2222
-'@ | Out-File -FilePath $sshConfigPath -Encoding utf8 
-  } 
+"@
+    $content | Out-File -FilePath $sshConfigPath -Encoding utf8
+  }
 }
 
-if ((Test-IsElevated) -eq $false) {
+if ((Test-IsElevated) -eq $False) {
   Write-Warning 'This script requires local admin privileges. Elevating...'
-  gsudo "& '$($MyInvocation.MyCommand.Source)'" $args
+  $forwardedArgs = foreach($boundParams in $PSBoundParameters.GetEnumerator()) {
+    if ($boundParams.Value -is [switch]) {
+      if ($boundParams.Value.IsPresent) { "-$($boundParams.Key)" }
+    }
+    else {
+      "-$($boundParams.Key)"
+      $boundParams.Value
+    }
+  }
+
+  gsudo "& '$($MyInvocation.MyCommand.Source)'" @forwardedArgs
   if ($LastExitCode -eq 999 ) {
     Write-error 'Failed to elevate.'
   }
@@ -188,5 +196,7 @@ if ((Test-IsElevated) -eq $false) {
 }
 
 Install-Docker -InstallDocker:$InstallDocker
+
+Install-BugayInitializeDocker -Build:$BugayInitializeDocker
 
 Initialize-LocalDockerContext -InitializeLocalDockerContext:$InitializeLocalDockerContext
